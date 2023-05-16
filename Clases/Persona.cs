a877d18e2e7bc0f1fabe159c805ac168c996e75d
RobertoZUpqroo <zetinaonline2k@gmail.com>
2023-05-16 12:20:32 -0500

    first commit

---
+++ Clases/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    // Clases es una plantilla 
+    // Que contiene Objetos y Metodos
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Tu nombre es: "+this.Nombre);
+            Console.WriteLine("Tu apellido es: "+this.Apellido);
+            Console.WriteLine("Tu edad es: "+this.Edad);
+            Console.WriteLine("El salario es: "+this.Salario);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            //instanciar es crear algo real
+
+            Persona persona = new Persona();
+
+            persona.Nombre = "Roberto";
+            persona.Apellido = "Zetina";
+            persona.Edad = 22;
+            persona.Salario = 10;
+
+            persona.ImprimirDatos();
+
+            Console.WriteLine("-------------------\n");
+            Persona persona2 = new Persona();
+
+            persona2.Nombre = "Axel";
+            persona2.Apellido = "Coutiño";
+            persona2.Edad = 22;
+            persona2.Salario = 10;
+
+            persona2.ImprimirDatos();
+
+            Console.WriteLine("------------------\n");
+
+            Persona persona3 = new Persona();
+
+            persona3.Nombre = "Adolf";
+            persona3.Apellido = "Hitler";
+            persona3.Edad = 45;
+
+            persona3.Salario = 1100000;
+
+            persona3.ImprimirDatos();
+            persona3.ImprimirDatos();
+            persona3.ImprimirDatos();
+            persona3.ImprimirDatos();
+            persona3.ImprimirDatos();
+            persona3.ImprimirDatos();
+
+        }
+    }
+}
